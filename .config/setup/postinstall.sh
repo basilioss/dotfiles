3ecdc9f241a301bec682dc6ea4294bfa63accf51
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2023-01-12 22:33:32 +0200

    Add pinch-to-zoom feature for Firefox on Xorg

--- .config/setup/postinstall.sh
+++ .config/setup/postinstall.sh
@@ -88,6 +88,11 @@ systemctl enable auto-cpufreq
 lockscreenWallpaper=$HOME/main/library/pictures/wallpapers/Louis-Coyle_Lakeside_2019_midnight.png
 betterlockscreen -u "$lockscreenWallpaper"
 
+# Smooth scrolling + MacOS/Windows like touchpad pinch-to-zoom in Firefox
+# https://www.reddit.com/r/linux/comments/72mfv8/psa_for_firefox_users_set_moz_use_xinput21_to/
+# https://www.reddit.com/r/linux/comments/rmuh0o/finally_macoswindows_like_touchpad_zoom_gesture/
+echo export MOZ_USE_XINPUT2=1 | sudo tee /etc/profile.d/use-xinput2.sh
+
 # Python packages
 
 # Remove background from images
