8883a636c87c59052f22a8fccf1e24211010f6fd
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2023-06-17 16:00:25 +0300

    Add /etc/profile.d/use-xinput2.sh

---
+++ .root/etc/profile.d/use-xinput2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Smooth scrolling + MacOS/Windows like touchpad pinch-to-zoom in Firefox
+# https://www.reddit.com/r/linux/comments/72mfv8/psa_for_firefox_users_set_moz_use_xinput21_to/
+# https://www.reddit.com/r/linux/comments/rmuh0o/finally_macoswindows_like_touchpad_zoom_gesture/
+export MOZ_USE_XINPUT2=1
