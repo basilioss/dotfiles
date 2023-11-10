b25bb1693c6303b36599edfa2b7717bb730b55d5
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2023-11-10 18:32:57 +0200

    Remove xresources and sxhkd autocmds (nvchad)

--- .config/nvchad/autocmds.lua
+++ .config/nvchad/autocmds.lua
@@ -6,18 +6,6 @@ autocmd("FileType", {
   command = "setlocal sw=2 ts=2 sts=2",
 })
 
--- restart sxhkd on save
-autocmd("BufWritePost", {
-  pattern = "*sxhkdrc",
-  command = "!pkill -USR1 sxhkd"
-})
-
--- merge xresources on save
-autocmd("BufWritePost", {
-  pattern = "xresources",
-  command = "!xrdb -merge ~/.config/x11/xresources"
-})
-
 -- disable auto comment
 autocmd("FileType", {
   command = "set formatoptions-=cro",
