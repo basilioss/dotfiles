e60285422201fd60b97d90125548ed15ba1196c0
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2023-11-12 20:33:13 +0200

    Hide status bar in neovim zen mode

--- .config/nvchad/plugins.lua
+++ .config/nvchad/plugins.lua
@@ -62,6 +62,9 @@ local plugins = {
           width = 85,
         },
         plugins = {
+          options = {
+            laststatus = 0,
+          },
           gitsigns = { enabled = true },
         },
       }
