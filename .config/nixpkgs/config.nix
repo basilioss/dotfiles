9b5516c311980323f7d1467d5bcdf221a676a80b
Basilios <vasyl.tyshchuk.xyz@gmail.com>
2024-04-30 23:14:33 +0300

    Add nix basic-packages

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,6 +2,26 @@
   # https://nixos.org/manual/nixpkgs/stable/#sec-declarative-package-management
   allowUnfree = true;
   packageOverrides = pkgs: with pkgs; {
+    basicPackages = pkgs.buildEnv {
+      name = "basic-packages";
+      paths = [
+        delta # A syntax-highlighting pager for git, diff, and grep output
+        fzf # A command-line fuzzy finder
+        helix # Text editor
+        lsd # Better ls
+        starship # Shell prompt
+        tmux # Terminal multiplexer
+        zoxide # Better cd
+        zsh
+        zsh-autosuggestions # Fish-like autosuggestions for zsh
+        zsh-fzf-tab # Replace zsh's default completion selection menu with fzf
+        zsh-history-substring-search # ZSH port of Fish history search (up arrow)
+        zsh-syntax-highlighting # Fish shell like syntax highlighting for Zsh
+        # ripgrep
+        # wslu # A collection of utilities for Windows Subsystem for Linux
+        # xdg-utils
+      ];
+    };
     myPackages = pkgs.buildEnv {
       name = "my-packages";
       paths = [
